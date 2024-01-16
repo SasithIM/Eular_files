6eb529fa9399ab8e4dbe015eb10f1914b481ee27
SasithIM <simahanama@gmail.com>
2024-01-16 18:49:18 +0530

    16.01.2024

---
+++ eu_65.py
@@ -0,0 +1,17 @@
+import fractions
+e=[2]
+n=33
+for i in range(n):
+    e.append(1)
+    e.append(2*(i+1))
+    e.append(1)
+
+e=e[::-1]
+last=1/e[0]
+
+for i in range(3*n):
+    next=e[i+1]+fractions.Fraction(1/last)
+    last=next
+
+print(sum([int(i) for i in str(next.numerator)]))
+# print(next.numerator)
